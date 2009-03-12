e90c785ea8a89adc56a1d215f86125fa2a20c30e
Andrew Shafer <andrew@reductivelabs.com>
2009-03-12 13:55:02 -0600

    Code and fix bitches, all the F'n time

--- manifests/enable.pp
+++ manifests/enable.pp
@@ -2,17 +2,17 @@ define passenger::enable($version) {
   passenger::install{ passenger: version => $version}
 
   file { passenger-load:
-         path => "/etc/apache2/mods-available/passenger.load"
-         content => "LoadModule passenger_module /usr/lib/ruby/gems/1.8/gems/passenger-$version/ext/apache2/mod_passenger.so",
+         path => "/etc/apache2/mods-available/passenger.load",
+         content => "LoadModule passenger_module /usr/lib/ruby/gems/1.8/gems/passenger-$version/ext/apache2/mod_passenger.so\n",
          mode => 644,
-         require => package['passenger']
+         require => Exec['passenger-install']
   }
 
   file { passenger-conf:
-         path => "/etc/apache2/mods-available/passenger.conf"
-         content => template(passenger.conf.erb),
+         path => "/etc/apache2/mods-available/passenger.conf",
+         content => template(passenger/passenger.conf.erb),
          mode => 644,
-         require => package['passenger']
+         require => Exec['passenger-install']
   }
 
   exec { a2enmod:
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,13 @@
 define passenger::install($version) {
   package { passenger:
             ensure => $version,
-            provider => gems,
+            provider => gem,
             require => [Class['gems'], Class['ruby'], Class['apache2']]
   }
 
   exec { passenger-install:
-         command => "/usr/bin/passenger-install-apache2-module"
+         command => "/usr/bin/yes \"\" | /usr/bin/passenger-install-apache2-module"
          creates => "/usr/lib/ruby/gems/1.8/gems/passenger-$version/ext/apache2/mod_passenger.so",
-         requires => Package['passenger']
+         require => Package['passenger']
   }
 }
