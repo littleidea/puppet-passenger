b7bc116baa3dc3e2a37dd54e29df5ee68681e671
Andrew Shafer <andrew@reductivelabs.com>
2009-03-13 00:17:00 -0600

    Refresh only

--- manifests/enable.pp
+++ manifests/enable.pp
@@ -6,17 +6,19 @@ define passenger::enable($version) {
          content => "LoadModule passenger_module /usr/lib/ruby/gems/1.8/gems/passenger-$version/ext/apache2/mod_passenger.so\n",
          mode => 644,
          require => Exec['passenger-install']
+         notify => Exec['a2enmod']
   }
 
   file { passenger-conf:
          path => "/etc/apache2/mods-available/passenger.conf",
          content => template("passenger/passenger.conf.erb"),
          mode => 644,
-         require => Exec['passenger-install']
+         require => Exec['passenger-install'],
+         notify => Exec['a2enmod']
   }
 
   exec { a2enmod:
          command => "/usr/sbin/a2enmod",
-         require => [File['passenger-load'], File['passenger-conf']]
+         refreshonly => true
   }
 }
